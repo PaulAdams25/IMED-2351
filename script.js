1a180a03327b9f4135ff558f112cb53e794e2aed
PaulAdams25 <35774883+PaulAdams25@users.noreply.github.com>
2020-10-26 12:07:25 -0500

    Add files via upload
    Typing test project that I still couldn't get to work.

--- script.js
+++ script.js
@@ -1,24 +1,79 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
-
-function runTheClock () {
-
-	var date = new Date();
-	console.log(date);
-	let hr = date.getHours();
-	let min = date.getMinutes();
-	let sec = date.getSeconds();
-	console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
-
-	let hrPosition = (hr*360/12)+(min*(360/60)/12);
-	let minPosition = (min*360/60)+(sec*(360/60)/60);
-	let secPosition = sec*360/60;
-
-	HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
-	MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
-	SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
-}
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
 
 
-var interval = setInterval(runTheClock,1000);
+var timer =[0,0,0,0];
+var interval;
+var timerRunning = false;
+
+// Add leading zero to numbers 9 or below (purely for aesthetics):
+	function leadingZero(time){
+		if (time <=9){
+			time = "0" + time;
+		}
+		return time;
+	}
+
+// Run a standard minute/second/hundredths timer:
+	function runTimer(){
+		let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2])
+		
+		theTimer.innerHTML = currentTime;
+		timer[3]++;
+		timer[0] = Math.floor((timer[3]/100)/60)
+		timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+		timer[2] = Math.floor(timer[3] - (timer[1] * 100) -(timer[0] * 6000))
+	}
+
+// Match the text entered with the provided text on the page:
+	function spellCheck(){
+		
+		let textEntered = testArea.value;
+		let originTextMatched = originText.substring(0, textEntered.length);
+		
+		if (textEntered == originText){
+			clearInterval(interval);
+			testWrapper.style.borderColor = "#429890";
+		}else{
+			if(textEntered == originTextMatched){
+			testWrapper.style.borderColor = "#65CCF3";	
+			}else{
+				testWrapper.style.borderColor = "#E95D0F";
+			}
+		}
+		
+		
+		
+	}
+
+// Start the timer:
+	function start() {
+		let textEnteredLength = testArea.value.length;
+		if (textEnteredLength === 0 && !timerRunning){
+			timerRunning = true;
+			interval = setInterval(runTimer,10);
+		}
+		
+	}
+
+// Reset everything:
+	function reset(){
+		clearInterval(interval);
+		interval = null;
+		timer = [0,0,0,0];
+		timerRunning = false;
+		
+		testArea.value = "";
+		theTimer.innerHTML = "00:00:00";
+		testWrapper.style.borderColor = "grey";
+	}
+
+// Event listeners for keyboard input and the reset button:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
+
+
